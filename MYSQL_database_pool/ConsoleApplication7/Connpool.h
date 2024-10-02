72d56b9181f3cc6d85150cfd5032a5ab6d62af8f
飞萤逐火 <2395477683@qq.com>
2024-10-02 15:51:36 +0800

    Add files via upload

---
+++ MYSQL_database_pool/ConsoleApplication7/Connpool.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<queue>
+#include<mutex>
+#include"Mysqlpool.h"
+#include<condition_variable>
+class Connpool
+{
+public:
+	static Connpool* getConnpool();
+	Connpool(const Connpool& obj)=delete;
+	Connpool& operator=(const Connpool& obj) = delete;
+	std::shared_ptr<Mysqlconn> getconnection();
+private:
+	Connpool();
+	~Connpool();
+	void producerconnection();
+	void recyclerconnection();
+
+	std::string my_ip="localhost";
+	std::string my_user="root";
+	std::string my_pass="macan344";
+	std::string my_dbname="test";
+	unsigned short my_port=3306;
+	int my_minsize=100;
+	int my_maxsize=1024;
+	int my_timeout=1000;
+	int my_maxIdleTime=5000;
+
+	std::queue<Mysqlconn*> my_connectionQueue;
+
+	std::mutex my_mutexQ;
+
+	std::condition_variable my_cond;
+};
+
